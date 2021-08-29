7091e9e29d55ccfc2552d38c8da3a9cfb632a328
Ahmad Alhashemi <ahmad@ahmadh.com>
2021-08-29 14:53:39 +0300

    Rename the project to swift-ml

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swinn",
+    name: "swift-ml",
     dependencies: [
         .package(url: "https://github.com/apple/swift-numerics", from: "0.1.0"),
     ],
@@ -12,12 +12,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "swinn",
+            name: "swift-ml",
             dependencies: [
                 .product(name: "Numerics", package: "swift-numerics"),
             ]),
-        .testTarget(
-            name: "swinnTests",
-            dependencies: ["swinn"]),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# swinn
+# swift-ml
 
-Hand-rolled neural network written in Swift. Implements the `backpropagation` algorithm described in Tom Mitchell's Machine Learning book (Ch 4, Table 4.2 on page 98 and section 4.6.4 on pages 106-108). 
+Experiments in implementing machine learning algorithms in Swift.
+
+Currently contains a hand-rolled neural network written in Swift. Implements the `backpropagation` algorithm described in Tom Mitchell's Machine Learning book (Ch 4, Table 4.2 on page 98 and section 4.6.4 on pages 106-108). 
